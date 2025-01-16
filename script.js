86b7e1e78ff1ec2437212edf27385b3b6f6015b5
chiamaka42 <udonsichiamaka@gmail.com>
2025-01-16 10:26:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Handle form submission
+document.querySelector('.email-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for signing up!');
+  });
+  
